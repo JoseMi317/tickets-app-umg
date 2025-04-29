5cd9038909306783735dc0a7d3ee567bc9927847
Jose Miguel <jmqc77@gmail,com>
2025-04-29 00:07:03 -0600

    create a base to ticket item

--- src/app/components/ticket-item/ticket-item.component.ts
+++ src/app/components/ticket-item/ticket-item.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Ticket } from '../../entities/ticket';
 
 @Component({
   selector: 'app-ticket-item',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './ticket-item.component.html',
 })
 export class TicketItemComponent {
+  @Input() ticket: Ticket | null = null;
 
+  get statusColor(): string {
+    if (!this.ticket) {
+      return 'bg-gray-400';
+    }
+
+    switch (this.ticket.status) {
+      case 'RESOLVED':
+        return 'bg-green-400';
+      case 'IN_PROGRESS':
+        return 'bg-yellow-400';
+      case  'RESOLVED':
+        return 'bg-red-400';
+      default:
+        return 'bg-gray-400'; // Si no hay status conocido
+    }
+  }
 }
