21a8a0c09253e3451c420f03589967311b1c95d5
angel_apkana <angel@apkana.com>
2025-04-28 22:38:47 -0600

    start agent form

--- src/app/components/tickets-form/tickets-form.component.ts
+++ src/app/components/tickets-form/tickets-form.component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-tickets-form',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './tickets-form.component.html',
-  styleUrl: './tickets-form.component.css'
 })
 export class TicketsFormComponent {
+  miFormulario: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+  ) {
+    this.miFormulario = this.fb.group({
+      nombre: [''],
+      email: [''],
+      telefono: [''],
+      mensaje: ['']
+    });
+  }
+
+  enviarDatos() {
+    if (this.miFormulario.valid) {
+      const datos = this.miFormulario.value;
+      //this.webSocketService.enviar(datos); // Aquí lo mandas al WebSocket
+    }
+  }
 
 }
