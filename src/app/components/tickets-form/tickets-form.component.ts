e39e344f6a753f5635d3fe6f6ce0daedc2405979
angel_apkana <angel@apkana.com>
2025-04-30 17:03:33 -0600

    fix form agent

--- src/app/components/tickets-form/tickets-form.component.ts
+++ src/app/components/tickets-form/tickets-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, inject } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-tickets-form',
@@ -7,28 +7,25 @@ import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
   templateUrl: './tickets-form.component.html',
 })
 export class TicketsFormComponent {
-  readonly agentForm: FormGroup;
   readonly fb = inject(FormBuilder);
 
-  constructor() {
-    this.agentForm = this.fb.group({
-      identification: [''],
-      firstName: [''],
-      lastName: [''],
-      phoneNumber: [''],
-      email: [''],
-      priority: [''],
-      status: [''],
-      description: [''],
-      createdAt: [''],
-      updatedAt: [''],
-      createdBy: [''], // Assuming it's just the user ID as a string
-    });
-  }
+  readonly ticketForm: FormGroup = this.fb.group({
+    identification: ['', Validators.required],
+    firstName: ['', Validators.required],
+    lastName: ['', Validators.required],
+    phoneNumber: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    status: ['', Validators.required],
+    priority: ['', Validators.required],
+    description: ['', Validators.required],
+    createdAt: [new Date(), Validators.required],
+    updatedAt: [new Date(), Validators.required],
+  });
+
 
   sendData() {
-    if (this.agentForm.valid) {
-      const datos = this.agentForm.value;
+    if (this.ticketForm.valid) {
+      const datos = this.ticketForm.value;
       //this.webSocketService.enviar(datos); // Aquí lo mandas al WebSocket
     }
   }
